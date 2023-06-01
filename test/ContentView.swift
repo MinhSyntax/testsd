def3d6dbef5a4719daf024b221f26c3e3aff916f
luc <luc.spielvogel@syntax-institut.de>
2023-06-01 13:20:46 +0200

    Luc added text n context view

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!asdjklasd")
             Text("MinhText")
+            Text("Luc Text")
         }
         .padding()
     }
